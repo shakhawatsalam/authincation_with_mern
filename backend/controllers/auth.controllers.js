5a3f047a7d4041e9e99a21db2ca9d3fde2245332
unknown <shakhawatsalam726@gmail.com>
2024-08-12 07:50:18 +0530

    [Verify Email and Logout Functionality is done]

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,8 +1,9 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
-import { sendVerificationEmail } from "../mailtrap/emails.js";
+import { sendVerificationEmail, sendWelcomeEmail } from "../mailtrap/emails.js";
 
+// * Sing up
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
   try {
@@ -50,9 +51,55 @@ export const signup = async (req, res) => {
     res.status(400).json({ success: false, message: error.message });
   }
 };
-export const login = async (req, res) => {
-  res.send("login route");
+
+// * Verify email
+export const verifyEmail = async (req, res) => {
+  const { code } = req.body;
+
+  try {
+    const user = await User.findOne({
+      verificationToken: code,
+      verificationTokenExpiresAt: { $gt: Date.now() },
+    });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid or expire verification code",
+      });
+    }
+    user.isVerified = true;
+    user.verificationToken = undefined;
+    user.verificationTokenExpiresAt = undefined;
+    await user.save();
+
+    await sendWelcomeEmail(user.email, user.name);
+    res.status(200).json({
+      success: true,
+      message: "Email verified successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    console.log("error in verifyEmail", error);
+    res.send(500).json({ success: false, message: "Server Error" });
+  }
 };
+// * Log in
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {}
+};
+// * Log out
 export const logout = async (req, res) => {
-  res.send("logout route");
+  res.clearCookie("token");
+  res.status(200).json({ success: true, message: "Logged out successfully" });
 };
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controllers.js";
+import {
+  login,
+  logout,
+  signup,
+  verifyEmail,
+} from "../controllers/auth.controllers.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+router.post("/verify-email", verifyEmail);
 
 export const authRouter = router;
