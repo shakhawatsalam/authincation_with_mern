adb73cf849e252096d8f00f9dc3f6e1e4e1d0e57
unknown <shakhawatsalam726@gmail.com>
2024-08-13 10:43:00 +0530

    {some changes}

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,24 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { connectDB } from "./db/connectDB.js";
 import { authRouter } from "./routes/auth.route.js";
-
+import path from "path";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json()); // * allows us to parse incoming request:req.body
 app.use(cookieParser()); // * allows us to parse incoming cookies
 
 app.use("/api/auth/", authRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
